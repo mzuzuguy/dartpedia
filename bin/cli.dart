08d9fb7e8d21454f61ecbeeeab84ad4cd20e95dd
mzuzuguy <preciousmusole18@gmail.com>
2026-06-23 08:39:24 +0200

    initial commit

---
+++ bin/cli.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+
+if (arguments.isEmpty || arguments.first == 'help') {
+  printUsage();
+
+} else if (arguments.first == 'version'){
+  print('Dartpedia CLT version $version');
+}else if(arguments.first == 'search'){
+  final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+
+}
+else {
+  printUsage();
+  }
+}
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search' <ARTICLE-TITLE>"
+  );
+}
+void searchWikipedia(List<String>? arguments){
+  print('searchWikipedia received arguments: $arguments');
+  final String articleTitle;
+
+  //if the user didn't pass in arguments, request an article tittle.
+  if (arguments == null || arguments.isEmpty) {
+    print('PLease provide an article title.');
+
+    articleTitle = stdin.readLineSync() ?? '';
+
+  }else {
+    articleTitle = arguments.join('  ');
+  }
+
+  print('Current article title: $articleTitle');
+
+  }

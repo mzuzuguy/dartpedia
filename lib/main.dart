df451a0f36c5e09c5cb550058fe30f1ce7ede6f8
mzuzuguy <preciousmusole18@gmail.com>
2026-06-26 15:46:03 +0200

    boxDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,32 @@
 import  'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'game.dart';
 
-void main(){
-  runApp(const Main());
-}
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final String letter;
+  final HitType hitType;
 
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World'),
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+        // TODO: add children
+      ),
+      child: Center(
+        child: Text(
+          letter.toLowerCase(),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
     );
